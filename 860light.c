a3c6a48c4ad07593b12ad803521d1eee8d1bac54
schneidera <schneidera@carleton.edu>
2017-03-06 15:01:28 -0600

    a few more changes to main camera

---
+++ 860light.c
@@ -0,0 +1,27 @@
+typedef struct lightLight lightLight;
+struct lightLight{
+    double translation[3];
+    double color[3];
+    double varying[3];
+};
+
+void lightInitialize(lightLight *light, double transl[3], double col[3]){
+    light->translation[0] = transl[0];
+    light->translation[1] = transl[1];
+    light->translation[2] = transl[2];
+    light->color[0] = col[0];
+    light->color[1] = col[1];
+    light->color[2] = col[2];
+}
+
+void updateTranslation(lightLight *light, double transl[3]){
+    light->translation[0] = transl[0];
+    light->translation[1] = transl[1];
+    light->translation[2] = transl[2];
+}
+
+void updateColor(lightLight *light, double col[3]){
+    light->color[0] = col[0];
+    light->color[1] = col[1];
+    light->color[2] = col[2];
+}
